469b26748be365674b509275c1f0c7637aa044cc
mdmahadih673 <mdmahadih673@gmail.com>
2026-09-17 02:01:02 +0600

    Creating Dynamic Layout and nested Layouts in Next.js

---
+++ src/app/Blog/layout.jsx
@@ -0,0 +1,11 @@
+import React, { Children } from 'react';
+
+const blogLayoutPage = ({ Children }) => {
+    return (
+        <div>
+            {Children}
+        </div>
+    );
+};
+
+export default blogLayoutPage;
